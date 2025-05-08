40f2bd903b71722f18936b530c95e48b5958ea19
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2025-05-09 08:59:59 +1000

    player movement

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -13,5 +13,51 @@ export default function makePlayer(k, posVec2, speed) {
     },
   ]);
 
+  let isMouseDown = false;
+  const game = document.getElementById("game");
+
+  game.addEventListener("focusout", () => {
+    isMouseDown = false;
+  });
+
+  game.addEventListener("mousedown", () => {
+    isMouseDown = true;
+  });
+
+  game.addEventListener("mouseup", () => {
+    isMouseDown = false;
+  });
+
+  game.addEventListener("touchstart", () => {
+    isMouseDown = true;
+  });
+
+  game.addEventListener("touchend", () => {
+    isMouseDown = false;
+  });
+
+  player.onUpdate(() => {
+    if (!k.camPos().eq(player.pos)) {
+      k.tween(
+        k.camPos(),
+        player.pos,
+        0.2,
+        (newPos) => k.camPos(newPos),
+        k.easings.linear
+      );
+    }
+
+    player.direction = k.vec2(0, 0);
+    const worldMousePos = k.toWorld(k.mousePos());
+
+    if (isMouseDown) {
+      player.direction = worldMousePos.sub(player.pos).unit();
+    }
+
+    // TODO: animations
+
+    player.move(player.direction.scale(speed));
+  });
+
   return player;
 }
