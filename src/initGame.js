d7a017647e6b4e252d747a94d1389f760298a78f
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2025-05-08 15:29:28 +1000

    finished tiledbackground

--- src/initGame.js
+++ src/initGame.js
@@ -1,4 +1,5 @@
 import makeKaplayCtx from "./kaplayCtx";
+import { PALETTE } from "./constants";
 
 export default async function initGame() {
   const k = makeKaplayCtx();
@@ -45,4 +46,18 @@ export default async function initGame() {
   k.loadSprite("kirby-ts", "./projects/kirby-ts.png");
   k.loadSprite("platformer-js", "./projects/platformer-js.png");
   k.loadShaderURL("tiledPattern", null, "./shaders/tiledPattern.frag");
+
+  const tiledBackground = k.add([
+    k.uvquad(k.width(), k.height()),
+    k.shader("tiledPattern", () => ({
+      u_time: k.time() / 20,
+      u_color1: k.Color.fromHex(PALETTE.color3),
+      u_color2: k.Color.fromHex(PALETTE.color2),
+      u_speed: k.vec2(1, -1),
+      u_aspect: k.width() / k.height(),
+      u_size: 5,
+    })),
+    k.pos(0),
+    k.fixed(),
+  ]);
 }
