f1d59246923859b5167cf6e623240875f331c397
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2025-05-12 15:38:46 +1000

    state management with jotai

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -1,4 +1,10 @@
 import { DIAGONAL_FACTOR } from "../constants";
+import {
+  isEmailModalVisibleAtom,
+  isProjectModalVisibleAtom,
+  isSocialModalVisibleAtom,
+  store,
+} from "../store";
 
 export default function makePlayer(k, posVec2, speed) {
   const player = k.add([
@@ -48,6 +54,13 @@ export default function makePlayer(k, posVec2, speed) {
         k.easings.linear
       );
     }
+
+    if (
+      store.get(isSocialModalVisibleAtom) ||
+      store.get(isEmailModalVisibleAtom) ||
+      store.get(isProjectModalVisibleAtom)
+    )
+      return;
 
     player.direction = k.vec2(0, 0);
     const worldMousePos = k.toWorld(k.mousePos());
